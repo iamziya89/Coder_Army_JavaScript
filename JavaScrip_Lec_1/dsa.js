04b5912da2f4e7c811ee1fa79db8d3b184a6ebce
Mohd Ziya <iamziya53@gmail.com>
2025-09-29 12:01:57 +0530

    Jsvascript dsa

--- JavaScrip_Lec_1/dsa.js
+++ JavaScrip_Lec_1/dsa.js
@@ -6,18 +6,185 @@
 //     console.log(i);  
 // }
 
-let arr=[11,12,13,14,15]
+// let arr=[11,12,13,14,15]
 // let rev=[]
 // for(let i=arr.length-1; i>=0 ; i--){
 //     rev.push(arr[i])
 // }
 // console.log(rev);
-for(let i=0; i<arr.length/2 ; i++){
-    var temp=arr[i];
-    arr[i]=arr[arr.length-1-i];
-    arr[arr.length-1-i]=temp;
-}
-console.log(arr);
+// for(let i=0; i<arr.length/2 ; i++){
+//     var temp=arr[i];
+//     arr[i]=arr[arr.length-1-i];
+//     arr[arr.length-1-i]=temp;
+// }
+// console.log(arr);
+
+
+
+// let obj={
+//     name:"Mohammad",
+//     age:23,
+//     email: "iamziya53@gmail.com"
+// }
+// for(let i in obj){
+//     console.log(i);
+    
+// }
+
+// let arr=[21,22,23,24,25]
+// let indexOf=arr.indexOf(23)
+// console.log(indexOf);
+// console.log(arr.includes(25));
+
+// let arr1=[1,2]
+// let arr2=[3,4]
+// // console.log(arr1.concat(arr2));
+// let arr3=[...arr2,...arr1]
+// console.log(arr3);
+
+// let arr=[5,2,9,1,25,28,12,30]
+// for(let i=0; i<arr.length ; i++){
+//     for(let j=0; j<arr.length ; j++){
+//         if(arr[j]>arr[j+1]){
+//             var temp=arr[j]
+//             arr[j]=arr[j+1]
+//             arr[j+1]=temp
+//         }
+//     }
+// }
+// console.log(arr);
+
+// let str="SHAMEEM"
+// let rev=""
+// for(let i=str.length-1; i>=0; i--){
+//     rev+=str[i]
+// }
+// console.log(rev);
+
+
+// function reverseStr(str){
+//     return str.split('').reverse().join('')
+// }
+// console.log(reverseStr("ANAS"));
+
+
+// function longestWord(sent){
+//     let long="";
+//     let word=sent.split(" ");
+//     for(let w of word){
+//         if(w.length>long.length){
+//             long=w;
+//         }
+//     }
+//     return long;
+// }
+// console.log(longestWord("I LOVE CODING IN JAVASCRIPT Shammeeemma abcdefghijklmnopq"));
+
+// PALLLINDROME
+// function pallindromeStr(str){
+//     let reverse=str.split('').reverse().join("");
+//     if(str===reverse) return `The given ${str} is PALLINDROME`
+//     else return `The given ${str} is NOT A PALLINDROME`
+// }
+// console.log(pallindromeStr("EYEA"));
+
+// INSERT ELEMENT IN ARRAY
+
+// let arr=[60,30,10,67,40]
+// let pos=3;
+// let newElm=1000;
+// for(let i=arr.length-1; i>=pos; i--){
+//     arr[i+1]=arr[i]
+//     if(i==pos){
+//         arr[i]=newElm;
+//     }
+// }
+// console.log(arr);
+
+// DELETE ELEMENT FROM AN ARRAY
+// let arr=[10,20,30,40,50,60]
+// let pos=2
+// for(let i=pos; i<arr.length; i++){
+//     arr[i]=arr[i+1]
+// }
+// arr.length=arr.length-1;
+// console.log(arr);
+
+// Search Element in Array
+// let arr=[20,30,10,5,50,100]
+// let item=10
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]==item) console.log(`the ${item} is present at index ${i} and position ${i+1}rd`);
+    
+// }
+
+// Wth Splice Method Delete Element
+// let arr=[20,30,10,5,50,100]
+// arr.splice(2,3)
+// console.log(arr);
+
+
+// Merging Two Arrays
+// let arr1=[3,7,12,34,56,90]
+// let arr2=[20,30,40,50]
+// let arr3=[]
+// for(let i=0; i<arr1.length; i++){
+//     arr3[i]=arr1[i]
+// }
+// for(let j=0; j<arr2.length; j++){
+//     arr3[j+arr1.length]=arr2[j]
+// }
+// console.log(arr3);
+
+// let p=1;
+// function fact(x){
+// if(x>0){
+//     console.log(x*p); 
+// }
+// fact(x-1)
+// }
+// fact(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
