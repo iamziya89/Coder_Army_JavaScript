fb481f94ce3a6fa196fddecbe21749057777b44c
Mohd Ziya <iamziya53@gmail.com>
2025-10-02 17:24:58 +0530

    Data Structure

---
+++ JavaScrip_Lec_1/stringanddateobj.js
@@ -0,0 +1,38 @@
+// const str=`Hello Coder Army`
+// console.log(str.length);
+// console.log(str[0]);
+// console.log(str.toLocaleUpperCase());
+
+// const now=new Date();
+// // console.log(now);
+// console.log(now.toString());
+
+// const dates=new Date();
+// console.log(dates.toLocaleString());
+// console.log(dates.getDate());
+// console.log(dates.getDay());
+// console.log(dates.getFullYear());
+// console.log(dates.getHours().toLocaleString());
+// console.log(dates.getMilliseconds());
+// console.log(dates.getUTCDay());
+// console.log(dates.toLocaleString());
+
+// const now=new Date(2025, 9, 2, 5, 20, 25,145)
+// console.log(now);
+// console.log(now.toLocaleString());
+
+const dates=Date.now();
+const now=new Date(1759405963385)
+
+console.log(now);
+
+
+
+
+
+
+
+
+
+
+
