abdcfb5916730bdf007d5630fec8480fda02191d
Mohd Ziya <iamziya53@gmail.com>
2025-10-06 23:55:21 +0530

    Object

--- JavaScrip_Lec_1/obj.js
+++ JavaScrip_Lec_1/obj.js
@@ -1,6 +1,29 @@
-const obj={
-    names:"Mohd Ziya",
-    age:25,
-    address:"223 Sitkahan Post Tanda Ambedkar Nagar UP 224190"
+const user={
+    name:"MohammadZiya",
+    age:29,
+    email:"iamziya53@gmail.com",
+    amount:5000,
 }
-console.log(obj);
+// console.log(user);
+// user.aadharr=969540745585;
+// user.amount=9000;
+// delete user.age;
+// delete user.email;
+// console.log(user);
+
+// console.log(user["amount"]);
+// const user2=user;
+// user2.age=50
+// console.log(user);
+// console.log(user2);
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+
+
+
+
+
+
+
+
