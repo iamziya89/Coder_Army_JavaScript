e646bf32fe19fbaf43c299dcdf392b703b660752
Mohd Ziya <iamziya53@gmail.com>
2025-10-08 03:34:01 +0530

    object practice

--- JavaScrip_Lec_1/func.js
+++ JavaScrip_Lec_1/func.js
@@ -28,6 +28,20 @@ const user={
     amount:5000,
 }
 
+// const {f,a}=user;
+// console.log(f,a);
+
+// const arr=[10,20,30,40,50]
+// const [f, l]=arr;
+// console.log(f, l);
+for(let i of Object.keys(user)){
+    console.log(user[i]);
+}
+for(let val of Object.values(user)){
+    console.log(val);
+}
+
+
 
 
 
