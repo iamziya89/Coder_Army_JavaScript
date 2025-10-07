5144600d735b0993b5ab3b44c531a56a1fce53d4
Mohd Ziya <iamziya53@gmail.com>
2025-10-07 19:46:56 +0530

    Object

--- JavaScrip_Lec_1/obj.js
+++ JavaScrip_Lec_1/obj.js
@@ -1,9 +1,9 @@
-const user={
-    name:"MohammadZiya",
-    age:29,
-    email:"iamziya53@gmail.com",
-    amount:5000,
-}
+// const user={
+//     name:"MohammadZiya",
+//     age:29,
+//     email:"iamziya53@gmail.com",
+//     amount:5000,
+// }
 // console.log(user);
 // user.aadharr=969540745585;
 // user.amount=9000;
@@ -16,9 +16,28 @@ const user={
 // user2.age=50
 // console.log(user);
 // console.log(user2);
-console.log(Object.keys(user));
-console.log(Object.values(user));
-console.log(Object.entries(user));
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+// console.log(Object.values(user));
+
+const user = {
+    name: "Mohammad Ziya",
+    age: 29,
+    email: "iamziya53@gmail.com",
+    amount: 5000,
+};
+
+// for(let x in user){
+//     console.log(x, user[x]);
+    
+// }
+for(let x in user){
+    console.log(x);
+    
+}
+
+
 
 
 
