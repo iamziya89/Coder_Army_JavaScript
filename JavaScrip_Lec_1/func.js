828c8f410b9225c15680243547f84015aead5c1d
Mohd Ziya <iamziya53@gmail.com>
2025-10-08 01:56:31 +0530

    object practice

--- JavaScrip_Lec_1/func.js
+++ JavaScrip_Lec_1/func.js
@@ -1,10 +1,35 @@
+// const user={
+//     fname:"Mohammad",
+//     lname:"Ziya",
+//     age:23,
+//     amount:5000,
+// }
+// console.log(user.lname);
+// user.amount=100000;
+// console.log(user.amount);
+// const user2=user;
+// user2.fname="Shameem Ahmad"
+// console.log(user);
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+// for(let key of user){
+//     console.log(key); 
+// }
+// const name=user.lname;
+// const age=user.age;
+// console.log(name,age);
+
 const user={
     fname:"Mohammad",
     lname:"Ziya",
     age:23,
     amount:5000,
 }
-// console.log(user.lname);
-user.amount=100000;
-console.log(user.amount);
+
+
+
+
+
 
