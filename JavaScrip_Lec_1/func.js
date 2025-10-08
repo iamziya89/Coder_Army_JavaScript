c8655c9d3cde0a00724e15b64e061b16ccd1f05f
Mohd Ziya <iamziya53@gmail.com>
2025-10-08 17:11:36 +0530

    object practice

--- JavaScrip_Lec_1/func.js
+++ JavaScrip_Lec_1/func.js
@@ -21,12 +21,12 @@
 // const age=user.age;
 // console.log(name,age);
 
-const user={
-    fname:"Mohammad",
-    lname:"Ziya",
-    age:23,
-    amount:5000,
-}
+// const user={
+//     fname:"Mohammad",
+//     lname:"Ziya",
+//     age:23,
+//     amount:5000,
+// }
 
 // const {f,a}=user;
 // console.log(f,a);
@@ -34,12 +34,85 @@ const user={
 // const arr=[10,20,30,40,50]
 // const [f, l]=arr;
 // console.log(f, l);
-for(let i of Object.keys(user)){
-    console.log(user[i]);
-}
-for(let val of Object.values(user)){
-    console.log(val);
-}
+// for(let i of Object.keys(user)){
+//     console.log(user[i]);
+// }
+// for(let val of Object.values(user)){
+//     console.log(val);
+// }
+
+//  const user={
+//     fname:"Mohammad",
+//     lname:"Ziya",
+//     age:23,
+//     amount:5000,
+// }
+// for(let [key,val] of Object.entries(user)){
+//     console.log(key,val);
+    
+// }
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+// for(let k of user){
+//     console.log(k);
+    
+// }
+
+// const name=user.fname;
+// const age=user.age;
+// console.log(name,age);
+// const {lname,age}=user;
+// console.log(lname,age);
+
+// let arr=[10,20,30,40,50,60]
+// let [f,s,t]=arr
+// console.log(f,s,t);
+
+// for(let key of Object.keys(user)){
+//     console.log(key); 
+// }
+// for(let val of Object.values(user)){
+//     console.log(val);
+// }
+
+// for(let [key,val] of Object.entries(user)){
+//     console.log(key,val); 
+// }
+
+// let user={
+//     fname : "Ziya",
+//     lname : "Shameem",
+//     age : 25,
+//     greet : function (){
+//         return (`Strike is coming soon ${this.lname}`);
+//     }
+// }
+// const va=user.greet();
+// console.log(va);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
