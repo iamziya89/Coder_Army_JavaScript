b00f587822a1446b7ce82f562e7ec54602cb30f7
Mohd Ziya <iamziya53@gmail.com>
2025-10-11 19:08:10 +0530

    DSA Practice

--- practicedsa.js
+++ practicedsa.js
@@ -6,11 +6,31 @@
 //     console.log(i);
 // }
 
-let arr=[11,12,13,14,15,16,17]
-for(let i=0; i<Math.floor((arr.length)/2); i++){
-    let temp=arr[i]
-    arr[i]=arr[arr.length-1-i]
-    arr[arr.length-1-i]=temp
-}
-console.log(arr);
+// let arr=[11,12,13,14,15,16,17]
+// for(let i=0; i<Math.floor((arr.length)/2); i++){
+//     let temp=arr[i]
+//     arr[i]=arr[arr.length-1-i]
+//     arr[arr.length-1-i]=temp
+// }
+// console.log(arr);
+
+// let obj={
+//     name : "Mohammad",
+//     age : 25,
+//     address : "223 Sitkahan Tanda Ambedkar Nagar - 224190"
+// }
+// console.log(obj["address"]);
+// console.log(obj["age"]);
+// console.log(obj.name);
+// console.log(Object.keys(obj)[1]);
+// console.log(Object.values(obj)[1]);
+// console.log(Object.entries(obj)[1]);
+
+// let x=Object.entries(obj)
+// for(let i=0; i<x.length; i++){
+//     for(let j=0; j<x[i].length; j++){
+//         console.log(x[i][j]); 
+//     }
+// }
+
 
